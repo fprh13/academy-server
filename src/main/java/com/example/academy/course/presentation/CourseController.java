ac928c18aa80f11ef675c1f2f99eea389732de97
조영무 <fprh13@gmail.com>
2026-05-26 02:38:46 +0900

    feat: 강의 목록 조회 API 작성 #16

--- src/main/java/com/example/academy/course/presentation/CourseController.java
+++ src/main/java/com/example/academy/course/presentation/CourseController.java
@@ -2,13 +2,17 @@ package com.example.academy.course.presentation;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.academy.common.presentation.dto.ApiResponse;
+import com.example.academy.common.presentation.dto.PagingRequest;
+import com.example.academy.common.presentation.dto.PagingResponse;
 import com.example.academy.course.application.CourseService;
 import com.example.academy.course.presentation.dto.request.RegisterCourseRequest;
 import com.example.academy.course.presentation.dto.response.CourseDetailResponse;
@@ -32,5 +36,13 @@ public class CourseController {
 	@GetMapping("/{courseId}")
 	public ResponseEntity<ApiResponse<CourseDetailResponse>> getCourse(@PathVariable Long courseId) {
 		return ResponseEntity.ok().body(ApiResponse.of(courseService.getCourseDetail(courseId)));
+	}
+
+	@GetMapping
+	public ResponseEntity<ApiResponse<PagingResponse<CourseDetailResponse>>> getCourses(
+		@ModelAttribute PagingRequest pagingRequest,
+		@RequestParam(required = false) String state
+	) {
+		return ResponseEntity.ok().body(ApiResponse.of(courseService.getCourses(state, pagingRequest)));
 	}
 }
